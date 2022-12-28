49967f7394bd0fe0c7ca90ba4444d32e305cf07c
Aleksandr-Popov-22 <43619329+Aleksandr-Popov-22@users.noreply.github.com>
2022-12-29 01:08:52 +0300

    Add files via upload

---
+++ lab_5/test-tdd.py
@@ -0,0 +1,21 @@
+from biqr_mod import sys, math, get_coef, get_roots
+import unittest
+
+class BiQrTest(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(get_roots(1,5,-36),[2,-2])
+
+
+    def test_2(self):
+        self.assertEqual(get_roots(3,5,6),[])
+
+
+    def test_3(self):
+
+        with self.assertRaises(ValueError):
+            get_coef('input')
+            
+if __name__ == '__main__':
+    unittest.main()
+         
